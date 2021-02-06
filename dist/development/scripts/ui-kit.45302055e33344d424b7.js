9d7f37bc825abe73a7e45f1fddb198bf361589ae
Anton Lykov <lykoffant@gmail.com>
2021-02-06 17:59:45 +0300

    Build this project from source

---
+++ dist/development/scripts/ui-kit.45302055e33344d424b7.js
@@ -0,0 +1,151 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/library.blocks/demo-section/demo-section.js":
+/*!*********************************************************!*\
+  !*** ./src/library.blocks/demo-section/demo-section.js ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _demo_section_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./demo-section.scss */ "./src/library.blocks/demo-section/demo-section.scss");
+
+
+/***/ }),
+
+/***/ "./src/library.blocks/heading-level-1/_hidden/heading-level-1_hidden.js":
+/*!******************************************************************************!*\
+  !*** ./src/library.blocks/heading-level-1/_hidden/heading-level-1_hidden.js ***!
+  \******************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _heading_level_1_hidden_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./heading-level-1_hidden.scss */ "./src/library.blocks/heading-level-1/_hidden/heading-level-1_hidden.scss");
+
+
+/***/ }),
+
+/***/ "./src/library.blocks/heading-level-1/heading-level-1.js":
+/*!***************************************************************!*\
+  !*** ./src/library.blocks/heading-level-1/heading-level-1.js ***!
+  \***************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _heading_level_1_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./heading-level-1.scss */ "./src/library.blocks/heading-level-1/heading-level-1.scss");
+/* harmony import */ var _hidden_heading_level_1_hidden_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./_hidden/heading-level-1_hidden.js */ "./src/library.blocks/heading-level-1/_hidden/heading-level-1_hidden.js");
+
+
+
+/***/ }),
+
+/***/ "./src/ui-kit.js":
+/*!***********************!*\
+  !*** ./src/ui-kit.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _theme_global_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./theme/global.scss */ "./src/theme/global.scss");
+/* harmony import */ var _library_blocks_heading_level_1_heading_level_1_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./library.blocks/heading-level-1/heading-level-1.js */ "./src/library.blocks/heading-level-1/heading-level-1.js");
+/* harmony import */ var _library_blocks_demo_section_demo_section_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./library.blocks/demo-section/demo-section.js */ "./src/library.blocks/demo-section/demo-section.js");
+
+
+
+
+/***/ }),
+
+/***/ "./src/library.blocks/demo-section/demo-section.scss":
+/*!***********************************************************!*\
+  !*** ./src/library.blocks/demo-section/demo-section.scss ***!
+  \***********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/library.blocks/heading-level-1/_hidden/heading-level-1_hidden.scss":
+/*!********************************************************************************!*\
+  !*** ./src/library.blocks/heading-level-1/_hidden/heading-level-1_hidden.scss ***!
+  \********************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/library.blocks/heading-level-1/heading-level-1.scss":
+/*!*****************************************************************!*\
+  !*** ./src/library.blocks/heading-level-1/heading-level-1.scss ***!
+  \*****************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/theme/global.scss":
+/*!*******************************!*\
+  !*** ./src/theme/global.scss ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/ui-kit.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
+//# sourceMappingURL=ui-kit.45302055e33344d424b7.js.map

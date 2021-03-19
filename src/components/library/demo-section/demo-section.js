3448c0a03bc8f2637c6ec6ed3bf4f1200395b2a2
Anton Lykov <lykoffant@gmail.com>
2021-03-19 18:15:13 +0300

    Add "demo-section" component

---
+++ src/components/library/demo-section/demo-section.js
@@ -0,0 +1,6 @@
+import './demo-section.scss';
+
+import './_theme/demo-section_theme_colors-and-type.scss';
+import './_theme/demo-section_theme_form-elements.scss';
+import './_theme/demo-section_theme_cards.scss';
+import './_theme/demo-section_theme_headers-and-footers.scss';
--- src/pages/ui-kit/ui-kit.js
+++ src/pages/ui-kit/ui-kit.js
@@ -3,3 +3,4 @@ import '@theme/global.scss';
 import '@components/library/body/body.js';
 import '@components/library/main/main.js';
 import '@components/library/title-1/title-1.js';
+import '@components/library/demo-section/demo-section.js';

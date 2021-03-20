550ca2a6be559cb84ea3a568e1ac06f4979ce2b0
Anton Lykov <lykoffant@gmail.com>
2021-03-20 16:34:56 +0300

    Add instances of the "title-2" component to the "UI kit" page

--- src/pages/ui-kit/ui-kit.js
+++ src/pages/ui-kit/ui-kit.js
@@ -4,3 +4,4 @@ import '@components/library/body/body.js';
 import '@components/library/main/main.js';
 import '@components/library/title-1/title-1.js';
 import '@components/library/demo-section/demo-section.js';
+import '@components/library/title-2/title-2.js';

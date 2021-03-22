44d9f9972f4c975d0380d291285237c3d8c8ee21
Anton Lykov <lykoffant@gmail.com>
2021-03-22 21:18:12 +0300

    Add "grid-of-colors-and-type" component to the UI kit page

--- src/pages/ui-kit/ui-kit.js
+++ src/pages/ui-kit/ui-kit.js
@@ -5,3 +5,4 @@ import '@components/library/main/main.js';
 import '@components/library/title-1/title-1.js';
 import '@components/library/demo-section/demo-section.js';
 import '@components/library/title-2/title-2.js';
+import '@components/library/grid-of-colors-and-type/grid-of-colors-and-type.js';
